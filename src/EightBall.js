9348414f74e49fdd205dabe73f3fc3eb136cfb22
Brian Shapiro <briandavidshapiro@gmail.com>
2020-12-30 03:01:32 -0500

    reset button working

--- src/EightBall.js
+++ src/EightBall.js
@@ -4,20 +4,30 @@ import "./EightBall.css";
 const EightBall = ({ answers }) => {
 	const [color, setColor] = useState("black");
 	const [text, setText] = useState("Think of a Question");
+
 	const genRandText = () => {
 		const rand = Math.floor(Math.random() * answers.length);
 		setColor(answers[rand].color);
 		return answers[rand].msg;
 	};
+
+	const reset = () => {
+		setText("Think of a Question");
+		setColor("black");
+	};
+
 	return (
-		<div
-			className={`EightBall EightBall-${color}`}
-			onClick={() => {
-				setText(genRandText(color));
-			}}
-		>
-			<h1 className="EightBall-message">{text}</h1>
-		</div>
+		<>
+			<div
+				className={`EightBall EightBall-${color}`}
+				onClick={() => {
+					setText(genRandText(color));
+				}}
+			>
+				<h1 className="EightBall-message">{text}</h1>
+			</div>
+			<button onClick={reset}>Reset</button>
+		</>
 	);
 };
 
